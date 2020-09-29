d7c97fb69e31ad43c7e842a03e247f5eb8f44b31
Darwin Martinez <darwin.oquely@yahoo.com>
2020-09-28 22:51:49 -0600

    Nueva Version de React JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+//import logo from './logo.svg';
+import "./styles/style.scss";
+import Curso from './Curso';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ()=>(
+<>
+  <div className="main-banner img-container l-section" id="main-banner">
+    <div className="ed-grid lg-grid-6">
+      <div className="lg-cols-4 lg-x-2">
+        <img className="main-banner__img" alt="Banner img" src="https://wallpaperaccess.com/full/1878506.jpg"/>
+        <div className="main-banner__data s-center">
+          <p className="t2 s-mb-0">Título del banner</p>
+          <p> Subtítulo del banner</p>
+          <a href="#" className="button">Botón del banner</a>
+        </div>
+      </div>
     </div>
-  );
-}
+  </div>
+
+  <div className="ed-grid m-grid-3">
+      
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+
+  </div>
+</>
+)
 
 export default App;
---
+++ src/Curso.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Curso = ()=>(
+    <article className="card">
+        <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+          <img src="https://ux.ed.team/img/curso-portada.png" alt=""/>
+        </div>
+        <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+          <h3 className="t5 s-mb-2 s-center">
+            Programación orientada a objetos con Go
+          </h3>
+          <div className="s-mb-2 s-main-center">
+            <div className="card__teacher s-cross-center">
+              <div className="card__avatar s-mr-1">
+                <div className="circle img-container">
+                  <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/66f015b2-0dfb-4ba9-bd0d-f7a7e1855275.jpeg" alt=""/>
+                </div>
+              </div>
+              <span className="small">Alexys Lozada</span>
+            </div>
+          </div>
+          <div className="s-main-center">
+            <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+          </div>
+        </div>
+      </article>
+)
+
+export default Curso;
