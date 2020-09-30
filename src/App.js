54ec01bc2f29b564d014953bbfb94c109d61a770
Darwin Martinez <darwin.oquely@yahoo.com>
2020-09-29 20:28:13 -0600

    codigo de la clase 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import React, {Fragment} from 'react';
 import "./styles/style.scss";
 import Curso from './Curso';
 
+const cursos = [
+  {"titulo": "React desde cero",
+   "imagen": "https://edteam-media.s3.amazonaws.com/courses/small/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg",
+  "profesor": "Darwin Martinez",
+  "precio": "20"}, 
+  {
+    "titulo": "HTML Completo",
+    "imagen": "https://edteam-media.s3.amazonaws.com/courses/small/26557907-0555-427e-a40c-6ff207f98d72.png",
+    "profesor": "Alexys Lozada",
+    "precio": "40"
+  }, 
+  {
+    "titulo": "JavaScritp La Guia Definitiva",
+    "imagen": "https://edteam-media.s3.amazonaws.com/courses/small/61e5a210-8dab-412e-a6dc-802c070cc18c.jpg",
+    "profesor": "Beto Quiroga",
+    "precio": "50"
+  }, 
+  {
+    "titulo": "Worpress",
+    "imagen": "https://edteam-media.s3.amazonaws.com/courses/small/82dfe34a-4e3b-4904-bdb5-03f45112fcd2.png",
+    "profesor": "Alexys Lozada",
+    "precio": "40"
+  }
+];
+
 const App = ()=>(
 <>
   <div className="main-banner img-container l-section" id="main-banner">
@@ -20,13 +45,11 @@ const App = ()=>(
 
   <div className="ed-grid m-grid-3">
       
-      <Curso/>
-      <Curso/>
-      <Curso/>
-      <Curso/>
-      <Curso/>
-      <Curso/>
+      {
+        cursos.map(curso => <Curso titulo={curso.titulo} imagen={curso.imagen} profesor={curso.profesor} precio={curso.precio}/>)
 
+        
+      }
   </div>
 </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,29 +1,46 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-const Curso = ()=>(
+/*const curso = {
+    "titulo": "react desde cero",
+    "imagen":"https://edteam-media.s3.amazonaws.com/courses/small/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg",
+    "precio": 50
+
+}*/
+
+const Curso = ({imagen, titulo, precio, profesor}) =>(
     <article className="card">
         <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-          <img src="https://ux.ed.team/img/curso-portada.png" alt=""/>
+          <img src={imagen} alt={titulo}/>
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-          <h3 className="t5 s-mb-2 s-center">
-            Programación orientada a objetos con Go
-          </h3>
-          <div className="s-mb-2 s-main-center">
-            <div className="card__teacher s-cross-center">
-              <div className="card__avatar s-mr-1">
-                <div className="circle img-container">
-                  <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/66f015b2-0dfb-4ba9-bd0d-f7a7e1855275.jpeg" alt=""/>
-                </div>
-              </div>
-              <span className="small">Alexys Lozada</span>
+          
+            <h3 className="center">{titulo}</h3>
+            <div className="s-main-center">
+                {`Prof. ${profesor}`}
+
             </div>
-          </div>
-          <div className="s-main-center">
-            <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
-          </div>
+        
+
+            <div className="s-main-center">
+    <a className="button--ghost-alert button--tiny" href="#">{`$ ${precio}`}</a>
+            </div>
         </div>
+        
       </article>
 )
 
+Curso.prototype={
+    titulo : propTypes.string,
+    imagen : propTypes.string,
+    profesor : propTypes.string,
+    precio : propTypes.string
+}
+
+Curso.defaultProps = {
+    titulo : "no definido",
+    imagen : "https://edteam-media.s3.amazonaws.com/courses/small/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg",
+    profesor : "no definido",
+    precio : "0"
+}
 export default Curso;
